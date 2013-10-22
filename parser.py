3eaa5572d13259c09bcba09d23fb53c31dc3d825
andrew.maach <andrew.maach@gmail.com>
2013-10-22 13:30:41 -0500

    extract verse, still working on chapter

--- parser.py
+++ parser.py
@@ -1,10 +1,35 @@
 from sys import argv
+import re
 
+_digits = re.compile('\d')
+def contains_digits(d):
+    return bool(_digits.search(d))
 
 f = open(argv[1])
+
+
+verse_count = 0
 
 for verse in f:
     if (verse.find("xxxx") > -1):
         continue
-    print(verse)
+    verse_count += 1
     
+    nverse = -1
+    nchapter = -1
+    
+    words = verse.split()
+    
+    for word in words:
+        if (contains_digits(word) == False):
+            continue
+        colon_index = word.find("׃")
+        if (colon_index == -1):
+            nverse = word
+        else:
+            nchapter = word[:colon_index]
+    
+    
+    if (verse_count == 5):
+        print(str(nchapter) + " & " + str(nverse))
+#    print(verse)
