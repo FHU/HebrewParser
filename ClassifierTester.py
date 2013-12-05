38281934e741b64091a8be301b9953018406fd23
Andrew Maach <andrew.maach@gmail.com>
2013-12-05 07:31:07 +0000

    Added normalization code. Isn't working for me, but should work...

--- ClassifierTester.py
+++ ClassifierTester.py
@@ -3,6 +3,7 @@ from sklearn import cross_validation
 from sklearn import datasets
 from sklearn import svm
 from sklearn import tree
+from sklearn import preprocessing
 import pandas
 
 class ClassifierTester:
@@ -20,9 +21,23 @@ class ClassifierTester:
 if __name__ == '__main__':
 	# Load Dataset
 	excel = pandas.ExcelFile("final_dataset.xlsx")
-	full_dataset = excel.parse("data")
-	dataset = full_dataset.drop(['Classification', 'Genre', 'hasElohim', "hasYHWH"], axis=1).values
-	labels = full_dataset['Classification'].values
+	parsedXlsx = excel.parse("data")
+	
+	leClass = preprocessing.LabelEncoder()
+	leClass.fit(['J', 'E', 'D', 'P'])
+	parsedXlsx['Classification'] = leClass.transform(parsedXlsx['Classification'])
+	
+	leGenre = preprocessing.LabelEncoder()
+	leGenre.fit(['Genealogy', 'Narrative', 'Covenant', 'Blessing', 'Law'])
+	parsedXlsx['Genre'] = leGenre.transform(parsedXlsx['Genre'])
+	
+	leName = preprocessing.LabelEncoder()
+	leName.fit(['Y', 'N'])
+	parsedXlsx['hasElohim'] = leName.transform(parsedXlsx['hasElohim'])
+	parsedXlsx['hasYHWH'] = leName.transform(parsedXlsx['hasYHWH'])
+	
+	dataset = parsedXlsx.drop(['Classification'], axis=1).values
+	labels = parsedXlsx['Classification'].values
 	clf = tree.DecisionTreeClassifier()
 
 	#Stratified K Fold 
@@ -32,10 +47,10 @@ if __name__ == '__main__':
 	print "Accuracy\tStandard Deviation"
 	print str(meanAccuracy) +"\t" + str(stdAccuracy)
 	print ""
-	
+	'''
 	#Leave One Out 
 	meanAccuracy, stdAccuracy = clfTester.LeaveOneOutTest(clf, dataset, labels)
 	print "Leave-One-Out Crossvalidation"
 	print "Accuracy\tStandard Deviation"
 	print str(meanAccuracy) +"\t" + str(stdAccuracy)
-
+'''
